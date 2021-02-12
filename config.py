79d7d2263252e05655cdd048381c0994e33ea843
anmolreddy <m.anmolreddy@gmail.com>
2021-02-12 19:25:46 +0530

    initial files

---
+++ config.py
@@ -0,0 +1,7 @@
+# database name, username, password, host, port number in order
+# update values if necessary
+name = "covid"
+user = "postgres"
+pswd = "ubuntu123"
+host = "127.0.0.1"
+port = "5432"
